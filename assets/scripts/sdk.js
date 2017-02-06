f6459d6dc85f5a5c1a6f0e945f99aa7c0b48164d
cjzamora <charles.andacc@gmail.com>
2017-02-06 17:39:18 +0800

    initial commit for sdk builder changes

--- assets/scripts/sdk.js
+++ assets/scripts/sdk.js
@@ -1,194 +1,57 @@
 (function() {
-    var redirect = true;
+    // Makes the sdk and libraries section
+    // of the documentation collabsilble
+    //
+    // For example: Turn the links from ...
+    //
+    // Android
+    //      SMS
+    //      USSD
+    //      ...
+    //
+    // To ..
+    //
+    // + Android
+    // + PHP
+    var Collapse = function() {
+        // on load make target links collapsible
+        $(window).on('load', function() {
+            setTimeout(function() {
+                // get the parent links
+                $('a[id^="sdks-libraries-"].level-2').each(function(index, el) {
+                    // customize text
+                    $(el)
+                        .removeAttr('href')
+                        .css('cursor', 'pointer')
+                        .html('+ ' + $(el).html());
 
-    var store = JSON.parse(document.getElementById('data-store').innerText);
+                    // hide the list
+                    $(el).next().toggle();
 
-    var update = function(size) {
-        document.querySelectorAll('footer.panel-footer img').forEach(function(image) {
-            image.parentElement.removeChild(image);
+                    // on link click
+                    $(el).on('click', function(e) {
+                        if($(el).hasClass('collapsed')) {
+                            // update text
+                            $(el)
+                                .removeClass('collapsed')
+                                .html($(el).html().replace('-', '+'));
+
+                            // hide the list
+                            $(el).next().toggle();
+                        } else {
+                            // update the text
+                            $(el)
+                                .addClass('collapsed')
+                                .html($(el).html().replace('+', '-'));
+
+                            // show the list
+                            $(el).next().toggle();
+                            // scroll to target
+                            $(el).next().find('li a:first').click();
+                        }
+                    });
+                });
+            }, 500);
         });
-
-        var choices = {}, platform = null;
-        document.querySelectorAll('input.choice').forEach(function(input) {
-            if(input.checked) {
-                var src = input.parentElement.querySelector('img').src;
-                var image = document.createElement('img');
-                image.src = src;
-
-                document.querySelector('footer.panel-footer span.choices').appendChild(image);
-
-                if(typeof store[input.value] !== 'undefined') {
-                    platform = input.value;
-                    choices.platform = platform;
-                    choices.api = [];
-                }
-
-                if(!platform || typeof store[platform][input.value] === 'undefined') {
-                    return;
-                }
-
-                choices.api.push(input.value);
-            }
-        });
-
-        if(!size) {
-            redirect = false;
-            document.querySelector('footer.panel-footer button').disabled = true;
-            document.querySelector('footer.panel-footer em').style.display = 'none';
-
-            return;
-        }
-
-        document.querySelector('footer.panel-footer em').innerHTML = size + 'KB';
-        document.querySelector('footer.panel-footer em').style.display = 'inline-block';
-
-        //determine the link
-        var download = '/sdk-generator/download.php';
-
-        switch(choices.platform) {
-            case 'android':
-                download += '?type=android';
-                break;
-            case 'ios':
-                download += '?type=ios';
-                break;
-            case 'react':
-                download += '?type=react';
-                break;
-            case 'phonegap':
-                download += '?type=phonegap';
-                break;
-        }
-
-        var files = []
-
-        files.push(choices.api.indexOf('sms') !== -1 ? 4: '');
-        files.push(choices.api.indexOf('voice') !== -1 ? null: '');
-        files.push(choices.api.indexOf('location') !== -1 ? 2: '');
-        files.push(choices.api.indexOf('charging') !== -1 ? 3: '');
-        files.push(choices.api.indexOf('ussd') !== -1 ? 6: '');
-        files.push(choices.api.indexOf('rewards') !== -1 ? '0': '');
-
-        var tmp = [];
-
-        for(var i in files) {
-            if(files[i] != '') {
-                tmp.push(files[i]);
-            }
-        }
-
-        files = tmp;
-
-        if(files.length > 1) {
-            files.push(1);
-        }
-
-        files = files.sort();
-
-        download += '&files=' + files.join('');
-
-        redirect = window.location.origin + download;
-
-        if(window.location.host === 'globelabs.github.io') {
-            redirect = 'http://ec2-54-254-220-204.ap-southeast-1.compute.amazonaws.com' + download;
-        }
-
-        document.querySelector('footer.panel-footer button').disabled = false;
-    };
-
-    var getSize = function() {
-        document.querySelector('footer.panel-footer button').disabled = true;
-
-        document.querySelectorAll('footer.panel-footer img').forEach(function(image) {
-            image.parentElement.removeChild(image);
-        });
-
-        var choices = {}, platform = null;
-        document.querySelectorAll('input.choice').forEach(function(input) {
-            if(input.checked) {
-                var src = input.parentElement.querySelector('img').src;
-                var image = document.createElement('img');
-                image.src = src;
-
-                document.querySelector('footer.panel-footer span.choices').appendChild(image);
-
-                if(typeof store[input.value] !== 'undefined') {
-                    platform = input.value;
-                    choices.platform = platform;
-                    choices.api = [];
-                }
-
-                if(!platform || typeof store[platform][input.value] === 'undefined') {
-                    return;
-                }
-
-                choices.api.push(input.value);
-            }
-        });
-
-        var files = []
-
-        files.push(choices.api.indexOf('sms') !== -1 ? 4: '');
-        files.push(choices.api.indexOf('voice') !== -1 ? null: '');
-        files.push(choices.api.indexOf('location') !== -1 ? 2: '');
-        files.push(choices.api.indexOf('charging') !== -1 ? 3: '');
-        files.push(choices.api.indexOf('ussd') !== -1 ? 6: '');
-        files.push(choices.api.indexOf('rewards') !== -1 ? '0': '');
-
-        var tmp = [];
-
-        for(var i in files) {
-            if(files[i] != '') {
-                tmp.push(files[i]);
-            }
-        }
-
-        files = tmp;
-
-        if(files.length > 1) {
-            files.push(1);
-        }
-
-        files = files.sort();
-
-        var key = choices.platform + '-' + files.join('');
-
-        if(window.sdkSize && choices.platform in window.sdkSize) {
-            if(key in window.sdkSize[choices.platform]) {
-                var size = window.sdkSize[choices.platform][key];
-
-                update(parseInt(size));
-
-                return;
-            }
-        }
-
-        update(null);
-    };
-
-    var success = function() {
-        if(!redirect) {
-            return;
-        }
-
-        window.open(redirect, '_blank');
-
-        //be responsible
-        document.querySelectorAll('input.choice').forEach(function(input) {
-            input.removeEventListener('change', getSize);
-        });
-
-        document.querySelector('footer.panel-footer button').removeEventListener('click', success);
-
-        //now we can swap out the form
-        var template = document.getElementById('success-template').innerHTML;
-        document.querySelector('div.sdk-builder-ui div.panel').innerHTML = template;
-    };
-
-    //drivers
-    document.querySelectorAll('input.choice').forEach(function(input) {
-        input.addEventListener('change', getSize);
-    });
-
-    document.querySelector('footer.panel-footer button').addEventListener('click', success);
+    }();
 })();
